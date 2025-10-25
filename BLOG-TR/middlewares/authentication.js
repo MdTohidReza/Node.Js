28eea1182af9fa51f36e1f8b2156b605ec53c6cf
Md Tohid Reza <harishalone786@gmail.com>
2025-10-25 21:45:14 +0545

    Blog

---
+++ BLOG-TR/middlewares/authentication.js
@@ -0,0 +1,22 @@
+const {validateToken} = require('../services/authentication');
+
+function checkForAuthenticationCookie(cookieName) {
+  return (req, res, next) => {
+    const tokenCookieValue = req.cookies[cookieName];
+    if (!tokenCookieValue)
+      return next();
+
+    try {
+      const userPayload = validateToken(tokenCookieValue);
+      req.user = userPayload;
+    } catch (error) {
+      console.error("Invalid token:", error.message);
+    }
+
+    return next(); // return ensures only one response flow
+  };
+}
+
+module.exports = {
+  checkForAuthenticationCookie,
+};
